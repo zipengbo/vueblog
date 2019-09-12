81d4c31a53fd32539653918fa92cb12ce1fb7b06
relx_pengbo <pengbo.zi@relxtech.com>
2019-09-12 17:28:20 +0800

    本地运行build环境

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     proxyTable: {
       '/': {
@@ -68,7 +68,7 @@ module.exports = {
         }
       }
     },
-    
+
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     //8080
